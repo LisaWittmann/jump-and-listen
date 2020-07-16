eeb156abeecc73b4ec688164ba9f18a635aa3e17
Lisa Wittmann <lisa.wittmann@student.hs-rm.de>
2020-07-16 17:05:20 +0200

    Schriftarten und Textformate

--- src/main/java/de/hsrm/mi/eibo/business/Test.java
+++
@@ -1,5 +0,0 @@
-package de.hsrm.mi.eibo.business;
-
-public class Test {
-    
-}
--- src/main/java/de/hsrm/mi/eibo/presentation/scenes/StartView.java
+++ src/main/java/de/hsrm/mi/eibo/presentation/scenes/StartView.java
@@ -1,10 +1,30 @@
 package de.hsrm.mi.eibo.presentation.scenes;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
-public class StartView extends AnchorPane {
+public class StartView extends VBox {
 
     public StartView(){
+        
+        Label titleLabel = new Label("Title");
+        titleLabel.getStyleClass().add("h1");
+
+        Label headerLabel = new Label("Header");
+        headerLabel.getStyleClass().add("h2");
+
+        Label textLabel = new Label("Normal Text");
+        textLabel.getStyleClass().add("normal-text");
+
+        setPadding(new Insets(100));
+        setSpacing(20);
+        setAlignment(Pos.CENTER);
+        getStyleClass().add("window");
+
+        getChildren().addAll(titleLabel, headerLabel, textLabel);
         
     }
     
