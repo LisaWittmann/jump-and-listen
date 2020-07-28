fa08a3fc7b1306dcc21e3668967e1cf12b1e38ae
lgers001 <lisa.gerstner@student.hs-rm.de>
2020-07-28 19:37:30 +0200

    Halbtoene hinzugefuegt, Hedwigs Theme

--- src/main/java/de/hsrm/mi/eibo/business/tone/IchBinZuTestzwecken.java
+++ src/main/java/de/hsrm/mi/eibo/business/tone/IchBinZuTestzwecken.java
@@ -15,8 +15,10 @@ public class IchBinZuTestzwecken {
         tm.setVolume(100);
         Song freudeSchoenerGoetterfunken = new Song(Level.INTERMEDIATE, "e e f g g f e d c c d e e d d e e f g g f e d c c d e d c c d d e c d e f e c d e f e d c d e e e f g g f e d c c d e d c c");
         Song alleMeineEntchen = new Song(Level.BEGINNER, "c d e f g g a a a a g a a a a g f f f f e e d d d d c");
+        Song hedwigsTheme = new Song(Level.EXPERT, "h e g fs e h a fs e g fs ds e h h e g fs e h d cs c gs c h as fs g e g h g h g c h as fs g h as as h h g h g h g d cs c gs c h as fs g e");
         songs.add(freudeSchoenerGoetterfunken);
         songs.add(alleMeineEntchen);
+        songs.add(hedwigsTheme);
         songPers.saveData(songs);
     }
 
--- src/main/java/de/hsrm/mi/eibo/business/tone/Tone.java
+++ src/main/java/de/hsrm/mi/eibo/business/tone/Tone.java
@@ -5,6 +5,7 @@ package de.hsrm.mi.eibo.business.tone;
  * @author pwieg001, lwitt001, lgers001
  */
 public enum Tone {
+    /*
     C(264),
     D(297),
     E(330),
@@ -12,6 +13,19 @@ public enum Tone {
     G(396),
     A(440),
     H(495);
+    */
+    C(262),
+    CS(277),
+    D(294),
+    DS(311),
+    E(330),
+    F(349),
+    FS(370),
+    G(392),
+    GS(415),
+    A(440),
+    AS(466),
+    H(494);
 
     private int freq;
     private Tone(int freq) {
