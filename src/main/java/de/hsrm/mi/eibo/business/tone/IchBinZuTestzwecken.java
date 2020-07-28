fa65ffb1a3a44292ce5b94a7cfb4de53c6437391
lgers001 <lisa.gerstner@student.hs-rm.de>
2020-07-28 17:15:55 +0200

    Alle meine Entchen 2

--- src/main/java/de/hsrm/mi/eibo/business/tone/IchBinZuTestzwecken.java
+++ src/main/java/de/hsrm/mi/eibo/business/tone/IchBinZuTestzwecken.java
@@ -1,5 +1,8 @@
 package de.hsrm.mi.eibo.business.tone;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.hsrm.mi.eibo.business.gamelogic.Level;
 import de.hsrm.mi.eibo.persistence.SongPersitinator;
 
@@ -8,11 +11,13 @@ public class IchBinZuTestzwecken {
     static SongPersitinator songPers = new SongPersitinator();
 
     public static void main(String[] args){
+        List<Song> songs = new ArrayList<Song>();
         tm.setVolume(100);
         Song freudeSchoenerGoetterfunken = new Song(Level.INTERMEDIATE, "e e f g g f e d c c d e e d d e e f g g f e d c c d e d c c d d e c d e f e c d e f e d c d e e e f g g f e d c c d e d c c");
         Song alleMeineEntchen = new Song(Level.BEGINNER, "c d e f g g a a a a g a a a a g f f f f e e d d d d c");
-        songPers.saveData(freudeSchoenerGoetterfunken);
-        songPers.saveData(alleMeineEntchen);
+        songs.add(freudeSchoenerGoetterfunken);
+        songs.add(alleMeineEntchen);
+        songPers.saveData(songs);
     }
 
 }
