1717e84ff3990874c900b67441272cb36a479826
Lisa Wittmann <lisa.wittmann@student.hs-rm.de>
2020-08-05 14:57:13 +0200

    G

--- src/main/java/de/hsrm/mi/eibo/business/tone/Tester.java
+++ src/main/java/de/hsrm/mi/eibo/business/tone/Tester.java
@@ -12,7 +12,7 @@ public class Tester {
         Song s3 = new Song("expert-false;hedwigs theme:H E G FS E H A FS E G FS DS E H H E G FS E H D CS C GS C H AS FS G E G H G H G C H AS FS G H AS AS H H G H G H G D CS C GS C H AS FS G E ");
         Song s4 = new Song("expert-false;vivaldi:C E E E D C G G F E E E D C G G F E F G F E D E C E E E D C G G F E E E D C G G F E F G F E D E G F E F G A G E G F E F G A G F E D C D C E G F E F G A G E G F E F G A G E A G F E D C D C C E E E D E F F E D D D C D E E F G G G G F E E E E F G G G F E D E G F E F G A G E G F E F G A G E A G F E D C D C ");
         Song s5 = new Song("intermediate-false;he is a pirate:C C D D D E F F F G E E D C C D C C D D D E F F F G E E D C D ");
-        Song s6 = new Song("beginner-false;jingle bells:E E E E E E E G C D E F F F F F E E E E D D E D ");
+        Song s6 = new Song("beginner-false;jingle bells:E E E E E E E G C D E F F F F F E E E E D D E D G ");
         songpers.saveData(s1);
         songpers.saveData(s2);
         songpers.saveData(s3);
